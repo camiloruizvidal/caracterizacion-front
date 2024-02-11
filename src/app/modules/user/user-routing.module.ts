79c81c762c30c4eb635a454de3c7bc8b82622b88
camiloruiz <camiloruizvidal@gmail.com>
2024-02-11 11:43:36 -0500

    Added usuarios

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
+import { ListComponent } from './components/list/list.component';
 
 const routes: Routes = [{
-  path: 'users',
+  path: '',
+  component: ListComponent
+},{
+  path: 'new',
+  component: UserComponent
+},{
+  path: 'update/:id',
   component: UserComponent
 }];
 
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -4,10 +4,14 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './components/user/user.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
 
 @NgModule({
   declarations: [
-    UserComponent
+    UserComponent,
+    ListComponent,
+    EditComponent
   ],
   imports: [
     CommonModule,
