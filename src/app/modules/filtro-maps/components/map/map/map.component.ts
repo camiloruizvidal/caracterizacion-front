619a15b21693efbba58d73e21aab4af0422f4fa6
camiloruiz <camiloruizvidal@gmail.com>
2024-02-20 10:18:07 -0500

    mapa

--- src/app/modules/filtro-maps/components/map/map/map.component.ts
+++ src/app/modules/filtro-maps/components/map/map/map.component.ts
@@ -3,20 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss'],
+  styleUrls: ['./map.component.scss']
 })
 export class MapComponent {
-  lat = 40.712776;
-  lng = -74.005974;
-  zoom = 10;
-
-  markers = [
-    { lat: 40.712776, lng: -74.005974, label: 'A', draggable: true },
-    { lat: 40.7291, lng: -73.9965, label: 'B', draggable: false },
-    { lat: 40.748817, lng: -73.985428, label: 'C', draggable: true },
-  ];
-
-  clickedMarker(label: string) {
-    console.log(`Marcador clickeado: ${label}`);
-  }
+  public zoom: number = 12;
+  public center!: google.maps.LatLngLiteral;
+  public options: google.maps.MapOptions = {
+    mapTypeId: 'hybrid',
+    zoomControl: false,
+    scrollwheel: false,
+    disableDoubleClickZoom: true,
+    maxZoom: 15,
+    minZoom: 8
+  };
 }
--- src/app/modules/filtro-maps/filtro-maps.module.ts
+++ src/app/modules/filtro-maps/filtro-maps.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './components/map/map/map.component';
 import { FiltroMapsRoutingModule } from './filtro-maps-routing.module';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @NgModule({
   declarations: [MapComponent],
-  imports: [CommonModule, FiltroMapsRoutingModule],
+  imports: [
+    CommonModule,
+    FiltroMapsRoutingModule,
+    GoogleMapsModule
+  ]
 })
 export class FiltroMapsModule {}
