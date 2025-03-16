d3682f8972f2577ced0f7e034756a45d64015859
camilo ruiz <camiloruizvidal@gmail.com>
2025-03-16 15:03:13 -0500

    Refactor alerta

--- src/app/modules/generador/generador.module.ts
+++ src/app/modules/generador/generador.module.ts
@@ -6,14 +6,9 @@ import { HelpersModule } from 'src/app/helpers/helpers.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { IsVisibleComponent } from './components/is-visible/is-visible.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AlertasComponent } from './components/alertas/alertas.component';
 
 @NgModule({
-  declarations: [
-    InputsGeneratorComponent,
-    IsVisibleComponent,
-    AlertasComponent
-  ],
+  declarations: [InputsGeneratorComponent, IsVisibleComponent],
   imports: [
     CommonModule,
     NgbModule,
