c9449286439f94fba1973a48b40244204288c77a
camilo ruiz <camiloruizvidal@gmail.com>
2025-03-19 20:07:11 -0500

    Actualizar AuthInterceptor para utilizar la URL de inicio de sesión desde el entorno y simplificar la lógica de verificación del token. Se eliminan registros de consola innecesarios, mejorando la claridad del código y la gestión de errores 401.

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -13,7 +13,7 @@ import { environment } from './../../../enviroment/enviroment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  private readonly loginUrl = '/api/v1/usuarios/login';
+  private readonly loginUrl = `${environment.apiUrl}/v1/usuarios/login`;
 
   constructor(private router: Router) {}
 
@@ -21,39 +21,23 @@ export class AuthInterceptor implements HttpInterceptor {
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    console.log('URL solicitada:', request.url);
-    console.log('¿Es URL de login?:', request.url.includes(this.loginUrl));
-
-    // Si es la ruta de login, no agregamos el token
-    if (request.url.includes(this.loginUrl)) {
+    if (request.url === this.loginUrl) {
       return next.handle(request);
     }
 
     const token = localStorage.getItem('token');
-    console.log('Token encontrado:', !!token);
 
     if (token) {
-      console.log('Agregando token a la petición');
       request = request.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
         }
       });
-      console.log(
-        'Headers después de agregar token:',
-        request.headers.get('Authorization')
-      );
-    } else {
-      console.log('No se encontró token en localStorage');
     }
 
     return next.handle(request).pipe(
       catchError((error: HttpErrorResponse) => {
-        console.log('Error en la petición:', error);
         if (error.status === 401) {
-          console.log(
-            'Error 401 - Limpiando localStorage y redirigiendo a login'
-          );
           localStorage.clear();
           this.router.navigate(['/login']);
         }
