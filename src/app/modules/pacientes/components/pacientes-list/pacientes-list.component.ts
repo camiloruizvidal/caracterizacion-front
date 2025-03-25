fe9ee971dbdea8f21d2b15d688f9509357cd1487
camilo ruiz <camiloruizvidal@gmail.com>
2025-03-25 08:10:25 -0500

    Cambiando el título de "Registro de pacientes" a "Mapeo de Excel" y eliminando la lógica de carga masiva en el componente de lista de pacientes, reemplazándola por el componente <app-mapeo-excel>.

--- src/app/modules/pacientes/components/pacientes-list/pacientes-list.component.ts
+++ src/app/modules/pacientes/components/pacientes-list/pacientes-list.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import { IPagination } from 'src/app/helpers/interface/interface';
-import { PacientesService } from '../../services/pacientes/pacientes.service';
-import { IPacienteId } from '../../interface/pacientes';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { NgbModalContent } from 'src/app/modules/helpers/modal.component/ngb-modal-content.component';
 
 @Component({
   selector: 'app-pacientes-list',
@@ -11,67 +6,5 @@ import { NgbModalContent } from 'src/app/modules/helpers/modal.component/ngb-mod
   styleUrls: ['./pacientes-list.component.scss']
 })
 export class PacientesListComponent {
-  public patientPagination!: IPagination<IPacienteId>;
-  public isShowUpload: boolean = false;
-  public selectedFile: File | null = null;
-  public estaSubiendoArchivo: boolean = false;
-
-  constructor(
-    private pacientesService: PacientesService,
-    private modalService: NgbModal
-  ) {}
-
-  public ngOnInit(): void {
-    this.loadPaginationPatients();
-  }
-
-  private loadPaginationPatients(
-    page: number = 1,
-    pageSize: number = 10
-  ): void {
-    this.pacientesService
-      .getPatients(page, pageSize)
-      .subscribe((response: IPagination<IPacienteId>) => {
-        this.patientPagination = response;
-      });
-  }
-
-  public onUpload() {
-    this.estaSubiendoArchivo = true;
-    if (this.selectedFile) {
-      const formData: FormData = new FormData();
-      formData.append('pacientes', this.selectedFile, 'pacientes');
-      this.pacientesService.sendFile(formData).subscribe(
-        (response: { message: string }) => {
-          this.estaSubiendoArchivo = false;
-          const modalRef = this.modalService.open(NgbModalContent, {
-            centered: true,
-            backdrop: 'static',
-            keyboard: false
-          });
-
-          modalRef.componentInstance.titulo = '¡Proceso Completado!';
-          modalRef.componentInstance.mensaje = response.message;
-        },
-        error => {
-          this.estaSubiendoArchivo = false;
-        }
-      );
-    }
-  }
-
-  public changePagination(value: {
-    itemsPerPage: number;
-    currentPage: number;
-  }): void {
-    this.loadPaginationPatients(value.currentPage, value.itemsPerPage);
-  }
-
-  public changeShowUpload(isShow: boolean) {
-    this.isShowUpload = isShow;
-  }
-
-  public onFileSelected(event: any) {
-    this.selectedFile = event.target.files[0];
-  }
+  constructor() {}
 }
