8bc09e65b0819508d87a38758895d37a2286ee5d
camilo ruiz <camiloruizvidal@gmail.com>
2025-03-25 09:00:48 -0500

    Agregando el componente VerCargaMasiva y su integración en la lista de pacientes, junto con su plantilla y estilos correspondientes.

---
+++ src/app/modules/pacientes/components/ver-carga-masiva/ver-carga-masiva.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ver-carga-masiva',
+  templateUrl: './ver-carga-masiva.component.html',
+  styleUrls: ['./ver-carga-masiva.component.scss']
+})
+export class VerCargaMasivaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/modules/pacientes/pacientes.module.ts
+++ src/app/modules/pacientes/pacientes.module.ts
@@ -4,6 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PacientesRoutingModule } from './pacientes-routing.module';
 import { PacientesListComponent } from './components/pacientes-list/pacientes-list.component';
 import { MapeoExcelComponent } from './components/mapeo-excel/mapeo-excel.component';
+import { VerCargaMasivaComponent } from './components/ver-carga-masiva/ver-carga-masiva.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InputsService } from '../generador/services/inputs.service';
 
@@ -13,7 +14,12 @@ import { HelpersModule } from 'src/app/helpers/helpers.module';
 import { FormulariosService } from '../formularios/services/formularios.service';
 
 @NgModule({
-  declarations: [PacientesListComponent, NgbModalContent, MapeoExcelComponent],
+  declarations: [
+    PacientesListComponent,
+    NgbModalContent,
+    MapeoExcelComponent,
+    VerCargaMasivaComponent
+  ],
   imports: [
     CommonModule,
     HelpersModule,
