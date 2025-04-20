ea0eed0f5ac757990891af7d3baaf126553f4888
camilo ruiz <camiloruizvidal@gmail.com>
2025-04-20 11:34:40 -0500

    Agregando el componente DateConditionSelector al módulo Generador, permitiendo la selección de condiciones de fecha relativas y absolutas. Se actualizan los componentes IsVisible y su plantilla para integrar el nuevo selector de condiciones de fecha, mejorando la funcionalidad del formulario.

--- src/app/modules/user/components/create/create.component.ts
+++ src/app/modules/user/components/create/create.component.ts
@@ -1,6 +1,7 @@
 import { IUserDetail } from '../../interface/user';
 import { UsersService } from '../../services/user/users.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,14 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.scss']
 })
 export class UserComponent {
-  constructor(private usersService: UsersService) {}
+  public showAlert = false;
+  public alertMessage = '';
+  public alertType = 'success';
+
+  constructor(private usersService: UsersService, private router: Router) {}
+
+  private showNotification(message: string, type: 'success' | 'danger') {
+    this.alertMessage = message;
+    this.alertType = type;
+    this.showAlert = true;
+    setTimeout(() => {
+      this.showAlert = false;
+      if (type === 'success') {
+        this.router.navigate(['/usuarios']);
+      }
+    }, 2000);
+  }
 
   public createNewUser(userData: IUserDetail) {
     this.usersService.createUser(userData).subscribe(
       response => {
-        console.log('Usuario creado exitosamente', response);
+        this.showNotification('Usuario creado exitosamente', 'success');
       },
       (error: any) => {
+        this.showNotification('Error al crear el usuario', 'danger');
         console.error('Error al crear el usuario', error);
       }
     );
--- src/app/modules/user/components/edit/edit.component.ts
+++ src/app/modules/user/components/edit/edit.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/user/users.service';
 import { IUserDetail } from '../../interface/user';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
@@ -11,10 +11,14 @@ import { ActivatedRoute } from '@angular/router';
 export class EditComponent implements OnInit {
   private id!: number;
   public user!: IUserDetail;
+  public showAlert = false;
+  public alertMessage = '';
+  public alertType = 'success';
 
   constructor(
     private usersService: UsersService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
   ) {
     this.route.params.subscribe(params => {
       this.id = +params['id'];
@@ -25,19 +29,38 @@ export class EditComponent implements OnInit {
     this.loadUser();
   }
 
+  private showNotification(message: string, type: 'success' | 'danger') {
+    this.alertMessage = message;
+    this.alertType = type;
+    this.showAlert = true;
+    setTimeout(() => {
+      this.showAlert = false;
+      if (type === 'success') {
+        this.router.navigate(['/usuarios']);
+      }
+    }, 2000);
+  }
+
   public loadUser(): void {
-    this.usersService.getUser(this.id).subscribe((user: IUserDetail) => {
-      this.user = user;
-    });
+    this.usersService.getUser(this.id).subscribe(
+      (user: IUserDetail) => {
+        this.user = user;
+      },
+      error => {
+        this.showNotification('Error al cargar el usuario', 'danger');
+        console.error('Error al cargar el usuario', error);
+      }
+    );
   }
 
   public editUser(userData: IUserDetail) {
     this.usersService.updateUser(this.id, userData).subscribe(
       response => {
-        console.log('Usuario creado exitosamente', response);
+        this.showNotification('Usuario actualizado exitosamente', 'success');
       },
       (error: any) => {
-        console.error('Error al crear el usuario', error);
+        this.showNotification('Error al actualizar el usuario', 'danger');
+        console.error('Error al actualizar el usuario', error);
       }
     );
   }
