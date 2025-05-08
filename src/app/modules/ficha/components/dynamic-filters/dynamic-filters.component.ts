85215de00c6df7f2c0ba1d83ba638551579da3d4
camilo ruiz <camiloruizvidal@gmail.com>
2025-05-08 12:39:50 -0500

    Agregando label

--- src/app/modules/ficha/components/dynamic-filters/dynamic-filters.component.ts
+++ src/app/modules/ficha/components/dynamic-filters/dynamic-filters.component.ts
@@ -34,6 +34,7 @@ export class DynamicFiltersComponent implements OnInit {
   public urlDescarga: string | null = null;
   public isGenerating: boolean = false;
   public currentFileName: string = '';
+  public resultado: IResultadoGenerarArchivoExcel | null = null;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -199,6 +200,7 @@ export class DynamicFiltersComponent implements OnInit {
     this.formulariosService.generarExcelTarjetasProcesadas().subscribe({
       next: (resultado: IResultadoGenerarArchivoExcel) => {
         this.currentFileName = resultado.fileName;
+        this.resultado = resultado;
         this.verificarEstadoExcel();
       },
       error: error => {
@@ -222,9 +224,8 @@ export class DynamicFiltersComponent implements OnInit {
           console.error('Error al generar el archivo');
         } else {
           setTimeout(() => {
-            console.log(123);
             this.verificarEstadoExcel();
-          }, 10000);
+          }, 1000);
         }
       },
       error => {
