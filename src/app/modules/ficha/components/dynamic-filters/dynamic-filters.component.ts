05cc26047b6d93fcc4148365d8af8840b3f838af
camilo ruiz <camiloruizvidal@gmail.com>
2025-05-08 12:49:36 -0500

    Revert "Agregando label"
    This reverts commit 58c513318d3904f86b651ab07c2194e0ea5485c6.

--- src/app/modules/ficha/components/dynamic-filters/dynamic-filters.component.ts
+++ src/app/modules/ficha/components/dynamic-filters/dynamic-filters.component.ts
@@ -14,11 +14,6 @@ import {
 } from 'src/app/modules/generador/interfaces/interface';
 import { ExportarService } from '../../services/exportar.service';
 import { NgxSpinnerService } from 'ngx-spinner';
-import { FormulariosService } from 'src/app/modules/formularios/services/formularios.service';
-import {
-  EFileStatus,
-  IResultadoGenerarArchivoExcel
-} from 'src/app/helpers/interface/interface';
 
 @Component({
   selector: 'app-dynamic-filters',
@@ -32,13 +27,10 @@ export class DynamicFiltersComponent implements OnInit {
   public filtrosForm: FormGroup;
   public filtros: IFiltrosBusqueda[] = [];
   public urlDescarga: string | null = null;
-  public isGenerating: boolean = false;
-  public currentFileName: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
-    private exportarService: ExportarService,
-    private formulariosService: FormulariosService
+    private exportarService: ExportarService
   ) {
     this.filtrosForm = this.formBuilder.group({
       tipoTarjeta: ['', Validators.required],
@@ -195,43 +187,14 @@ export class DynamicFiltersComponent implements OnInit {
       return;
     }
 
-    this.isGenerating = true;
-    this.formulariosService.generarExcelTarjetasProcesadas().subscribe({
-      next: (resultado: IResultadoGenerarArchivoExcel) => {
-        this.currentFileName = resultado.fileName;
-        this.verificarEstadoExcel();
-      },
-      error: error => {
-        this.isGenerating = false;
-        console.error(error);
-      }
-    });
-  }
+    this.exportarService
+      .exportarFicha(this.tarjetaJson.version.toString())
+      .subscribe({
+        next: response => {},
 
-  private verificarEstadoExcel() {
-    if (!this.currentFileName) return;
-
-    this.formulariosService.validarEstadoExcel(this.currentFileName).subscribe(
-      (resultado: { estado: EFileStatus }) => {
-        if (resultado.estado === EFileStatus.COMPLETED) {
-          this.isGenerating = false;
-          this.currentFileName = '';
-        } else if (resultado.estado === EFileStatus.NOT_STARTED) {
-          this.isGenerating = false;
-          this.currentFileName = '';
-          console.error('Error al generar el archivo');
-        } else {
-          setTimeout(() => {
-            console.log(123);
-            this.verificarEstadoExcel();
-          }, 10000);
+        error: error => {
+          console.error(error);
         }
-      },
-      error => {
-        this.isGenerating = false;
-        this.currentFileName = '';
-        console.error('Error al verificar estado:', error);
-      }
-    );
+      });
   }
 }
